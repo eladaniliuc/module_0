5fe6b7bd21a91135f3f84f028a855ee01cd8ae3b
eladaniliuc <81648573+eladaniliuc@users.noreply.github.com>
2021-03-31 12:30:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ number = np.random.randint(1,101)      # загадали число
 print ("Загадано число от 1 до 100")
                 
 def score_game(game_core):
-    '''Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число'''
+    '''Запускаем игру 100 раз, чтобы узнать, как быстро игра угадывает число'''
     count_ls = []
     np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
-    random_array = np.random.randint(1,101, size=(1000))
+    random_array = np.random.randint(1,101, size=(100))
     for number in random_array:
         count_ls.append(game_core(number))
     score = int(np.mean(count_ls))
